5b6c0cd57aa81dc40ccc3848cae39287d5284ab8
Ali Zohdi <alii.zohdii@gmail.com>
2023-09-30 15:20:43 +0330

    all sub_industries are gathered

--- Saba_Scrapping.py
+++ Saba_Scrapping.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import time
 
 url = 'https://infosaba.com/industries/آهن-و-فولاد'
 s = Service(r"Z:\\HQ\BDM\\a.zohdi\\Data Engineering\\Github\\Geocoding\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe")
@@ -19,29 +20,27 @@ def sub_industries():
     print('title', hrefs)
 
     sub_ind_id = 0
+    sub_industry_list = []
     while sub_ind_id in range(len(hrefs)):
         sub_ind_id += 1
-        print(sub_ind_id)
-        
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         sub_industry_click = driver.find_element(By.XPATH, f'//*[@id="mainPageContainer"]/div/div[2]/div/a[{sub_ind_id}]/div/span')
         sub_industry_click.click()
-#       try:
+       
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
         elements = soup.find_all(class_='d-flex m-1')
         hrefs_2 = [element.find('h3').text.strip() for element in elements]
-        # if len(hrefs_2) == 0:
-        #     print("no sub industry")
-        # else:   
-        print(hrefs_2)
-        driver.implicitly_wait(400)
-        driver.back()
-        driver.implicitly_wait(400)            
-        # except:
-        #     # header = driver.find_element(By.XPATH, '//*[@id="ajaxcontent"]/div[1]/h3')
-        #     # print(header.text)    
-        #     print("no sub industry!!!!")
-        #     driver.back()
+
+        if len(hrefs_2) == 0:
+            sub_industry_list.append([hrefs[sub_ind_id - 1], hrefs[sub_ind_id]])
+        else:
+            for i in range(len(hrefs_2)):
+                sub_industry_list.append([hrefs[sub_ind_id - 1], hrefs_2[i]])    
+   
+        print(sub_industry_list)
+        driver.back()           
 
 
 sub_industries()

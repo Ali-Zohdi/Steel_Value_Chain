2d5c4abcef665ca04730c9b337ad245b824b1b96
Ali Zohdi <alii.zohdii@gmail.com>
2023-10-05 10:41:58 +0330

    route full function

--- routing_osrm.py
+++ routing_osrm.py
@@ -1,10 +1,42 @@
 import requests
+from typing import Union
 
-def OSRM_routes(coords_1, coords_2):
+def route(coordinates , alternatives : Union[int,str] ="false", steps=False, annotations : Union[int,str] ="false", geometries="polyline", overview="simplified"):
+
+    alternatives_url = steps_url = geometries_url = annotations_url = overview_url = ""
+
+    if alternatives == "false":
+        pass
+    else:
+        alternatives_url = f"&alternatives={str(alternatives)}"
+    
+    if steps:
+        steps_url = f"&steps=true"
+    
+    geometries_url = f"?geometries={geometries}"
+
+    if annotations == "false":
+        pass
+    else:
+        annotations_url = f"&annotations={annotations}"
+
+    if overview == "simplified":
+        pass
+    else:
+        overview_url = f"&overview={overview}"
+
+    if len(coordinates) >= 2 :
+        coordinates_url = ""
+        for coordinate in coordinates:
+            coordinates_url += f"{coordinate[1]},{coordinate[0]};"
+        coordinates_url = coordinates_url[:-1]
+    else:
+        return "Error: less than 2 coordinates has been given"
 
     osrm_url = "http://router.project-osrm.org/route/v1/driving/"
-    request_url = f"{osrm_url}{coords_1[1]},{coords_1[0]};{coords_2[1]},{coords_2[0]}"
+    request_url = f"{osrm_url}{coordinates_url}{geometries_url}{alternatives_url}{steps_url}{annotations_url}{overview_url}"
     response = requests.get(request_url)
+    print(request_url)
 
     if response.status_code == 200 :
         
@@ -15,3 +47,4 @@ def OSRM_routes(coords_1, coords_2):
     else:
         print("Response status code is not 200!")
 
+    return data

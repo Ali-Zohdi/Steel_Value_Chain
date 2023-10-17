49ba5fdce145c897410a83ae3f723bfb6990f4f4
Ali Zohdi <alii.zohdii@gmail.com>
2023-10-17 10:54:24 +0330

    route fun ET

--- routing_osrm.py
+++ routing_osrm.py
@@ -1,10 +1,42 @@
 import requests
 from typing import Union
+import polyline
+import folium
+import numpy as np
 
-def route(coordinates , alternatives : Union[int,str] ="false", steps=False, annotations : Union[int,str] ="false", geometries="polyline", overview="simplified"):
+def route(coordinates , alternatives : Union[int,str] ="false", steps=False, annotations="false", geometries="polyline", overview="simplified"):
+    """
+    Parameters
+    ----------
+    coordinates : List
+        A list of coordinations that specifies origin and destination of the route. It can be more than 2 points.
+    
+    alternatives : "false" (default), "true", int
+        Search for alternative routes. Passing a number alternatives=n searches for up to n alternative routes. Even if alternative routes are requested, a result cannot be guaranteed.
+    
+    steps: False (default), True
+        Returned route steps for each route leg
+    
+    annotations : "false" (default), "true", "nodes", "distance", "duration", "datasources", "weight", "speed"
+        Returns additional metadata for each coordinate along the route geometry.
+    
+    geometries : "polyline" (default), "polyline6", "geojson"
+        Returned route geometry format (influences overview and per step).
+    
+    overview : "simplified" (default), "full", "false"
+        Add overview geometry either full, simplified according to highest zoom level it could be display on, or not at all.
 
+    Returns
+    -------
+    result[0] : dict
+        A dictionary which is a transformed version of the response that contains route(s) values
+    result[1] : json
+        Full response of the request as a json file
+    
+    """
+    ## EXTRACT ##
 
-    ## Checking parameters of the request
+        ## Checking parameters of the request
     alternatives_url = steps_url = geometries_url = annotations_url = overview_url = ""
 
     if alternatives == "false":
@@ -35,18 +67,49 @@ def route(coordinates , alternatives : Union[int,str] ="false", steps=False, ann
     else:
         return "Error: less than 2 coordinates has been given"
 
+        ## Creating request
     osrm_url = "http://router.project-osrm.org/route/v1/driving/"
     request_url = f"{osrm_url}{coordinates_url}{geometries_url}{alternatives_url}{steps_url}{annotations_url}{overview_url}"
     response = requests.get(request_url)
     print(request_url)
 
-    if response.status_code == 200 :
-        
-        data = response.json()
+    if response.status_code != 200:
+        return "Error: The response status code is not 200"
+    
+    response = response.json()
 
-        distance = data['routes'][0]['distance']
-        print(f"The distance using OSRM is: {distance}")
-    else:
-        print("Response status code is not 200!")
+    ## TRANSFORM ##
 
-    return data
+    routes = {'start_point_latitude': [],
+              'start_point_longitude': [],
+              'end_point_latitude': [],
+              'end_point_longitude': [],
+              'route_number': [],
+              'geometry': [],
+              'route_coordinates': [],
+              'distance': [],
+              'duration': []}
+    
+    i = 0
+    while i < len(response):
+
+        routes['start_point_latitude'].append(coordinates[0][0])
+        routes['start_point_longitude'].append(coordinates[0][1])
+        routes['end_point_latitude'].append(coordinates[-1][0])
+        routes['end_point_longitude'].append(coordinates[-1][1])
+        routes['route_number'].append(i+1)
+        if geometries == 'geojson':
+            routes['geometry'].append(np.nan)
+            routes['route_coordinates'].append(response['routes'][i]['geometry']['coordinates'])
+        elif geometries == 'polyline6':
+            routes['geometry'].append(response['routes'][i]['geometry'])
+            routes['route_coordinates'].append(np.nan)
+        else:
+            routes['geometry'].append(response['routes'][i]['geometry'])
+            routes['route_coordinates'].append(polyline.decode(response['routes'][i]['geometry']))
+        routes['distance'].append(response['routes'][i]['distance'])
+        routes['duration'].append(response['routes'][i]['duration'])
+
+        i += 1
+
+    return routes, response

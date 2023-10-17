c9c82e2246da4930963ee510b91797acb0e3e47a
Ali Zohdi <alii.zohdii@gmail.com>
2023-10-17 14:55:11 +0330

    distance matrix added

--- routing_osrm.py
+++ routing_osrm.py
@@ -3,13 +3,16 @@ from typing import Union
 import polyline
 import folium
 import numpy as np
+import pandas as pd
 
 def route(coordinates , alternatives : Union[int,str] ="false", steps=False, annotations="false", geometries="polyline", overview="simplified"):
     """
+    Finds the fastest route between coordinates in the supplied order.
+
     Parameters
     ----------
     coordinates : List
-        A list of coordinations that specifies origin and destination of the route. It can be more than 2 points.
+        A list of lat-long coordinations (lat, long) that specifies origin and destination of the route. It can be more than 2 points.
     
     alternatives : "false" (default), "true", int
         Search for alternative routes. Passing a number alternatives=n searches for up to n alternative routes. Even if alternative routes are requested, a result cannot be guaranteed.
@@ -28,13 +31,15 @@ def route(coordinates , alternatives : Union[int,str] ="false", steps=False, ann
 
     Returns
     -------
-    result[0] : dict
+    result['routes'] : dict
         A dictionary which is a transformed version of the response that contains route(s) values
-    result[1] : json
-        Full response of the request as a json file
+    result['response'] : json
+        Full response of the request
     
     """
+    #############
     ## EXTRACT ##
+    #############
 
         ## Checking parameters of the request
     alternatives_url = steps_url = geometries_url = annotations_url = overview_url = ""
@@ -65,20 +70,21 @@ def route(coordinates , alternatives : Union[int,str] ="false", steps=False, ann
             coordinates_url += f"{coordinate[1]},{coordinate[0]};"
         coordinates_url = coordinates_url[:-1]
     else:
-        return "Error: less than 2 coordinates has been given"
+        return print("Error: less than 2 coordinates has been given")
 
         ## Creating request
     osrm_url = "http://router.project-osrm.org/route/v1/driving/"
     request_url = f"{osrm_url}{coordinates_url}{geometries_url}{alternatives_url}{steps_url}{annotations_url}{overview_url}"
     response = requests.get(request_url)
-    print(request_url)
 
     if response.status_code != 200:
-        return "Error: The response status code is not 200"
+        return print(f"Error: The response status code is not 200 for {request_url}")
     
     response = response.json()
 
+    ###############
     ## TRANSFORM ##
+    ###############
 
     routes = {'start_point_latitude': [],
               'start_point_longitude': [],
@@ -91,7 +97,7 @@ def route(coordinates , alternatives : Union[int,str] ="false", steps=False, ann
               'duration': []}
     
     i = 0
-    while i < len(response):
+    while i < len(response['routes']):
 
         routes['start_point_latitude'].append(coordinates[0][0])
         routes['start_point_longitude'].append(coordinates[0][1])
@@ -112,4 +118,104 @@ def route(coordinates , alternatives : Union[int,str] ="false", steps=False, ann
 
         i += 1
 
-    return routes, response
+    result = {'routes': routes,
+              'response': response}
+
+    return result
+
+
+def table(sources : list, destinations : list, annotations='distance,duration'):
+    """
+    Computes the duration of the fastest route between all pairs of supplied coordinates. Returns the durations or distances or both between the coordinate pairs. Note that the distances are not the shortest distance between two coordinates, but rather the distances of the fastest routes. Duration is in seconds and distances is in meters.
+
+    Parameters
+    ----------
+    sources : list
+        A list of lat-long coordinates (lat, long)
+
+    destinations : list
+        A list of lat-long coordinates (lat, long)
+    
+    annotation : "distance,duration" (default), "distance", "duration"
+        Return the requested table or tables in response.
+
+    Return
+    ------
+    result['distance_m'] : DataFrame
+        Distance table in Meters
+
+    result['distance_km'] : DataFrame
+        Distance table in Kiloeters
+    
+    result['duration_min'] : DataFrame
+        Duration table in Minutes
+
+    result['duration_h'] : DataFrame
+        Duration table in Hours
+
+    result['response'] : json
+        Full response of the request
+    """
+    #############
+    ## EXTRACT ##
+    #############
+
+        ## Checking parameters of the request
+    annotations_url = f"?annotations={annotations}"
+
+    if len(sources) == 0 or len(destinations) == 0:
+        return print("Error: sources or destinations list is null")
+    else:
+        coordinates_url = ""
+        sources_url = "&sources="
+        destinations_url = "&destinations="
+        
+        i = 0
+        while i < len(sources):
+            coordinates_url += f"{sources[i][1]},{sources[i][0]};"
+            sources_url += f"{i};"
+            i += 1
+        
+        j = 0
+        while j < len(destinations):
+            coordinates_url += f"{destinations[j][1]},{destinations[j][0]};"
+            destinations_url += f"{i+j};"
+            j += 1
+
+    coordinates_url = coordinates_url[:-1]
+    sources_url = sources_url[:-1]
+    destinations_url = destinations_url[:-1]
+
+        ## Creating request
+    osrm_url = "http://router.project-osrm.org/table/v1/driving/"
+    request_url = f"{osrm_url}{coordinates_url}{annotations_url}{sources_url}{destinations_url}"
+    response = requests.get(request_url)
+
+    if response.status_code != 200:
+        return print(f"Error: The response status code is not 200 for {request_url}")
+    
+    response = response.json()
+            
+    ###############
+    ## TRANSFORM ##
+    ###############
+
+    distance_m = response['distances']
+    duration_sec = response['durations']
+
+    distance_km = [[x / 1000 for x in row] for row in distance_m]
+    duration_min = [[x / 60 for x in row] for row in duration_sec]
+    duration_h = [[x / 60 for x in row] for row in duration_min]
+
+    distance_m = pd.DataFrame(distance_m, index=sources, columns=destinations)
+    distance_km = pd.DataFrame(distance_km, index=sources, columns=destinations)
+    duration_min = pd.DataFrame(duration_min, index=sources, columns=destinations)
+    duration_h = pd.DataFrame(duration_h, index=sources, columns=destinations)
+
+    result = {'distance_m': distance_m,
+              'distance_km': distance_km,
+              'duration_min': duration_min,
+              'duration_h': duration_h,
+              'response': response}
+
+    return result
